19381b8ae0ee7e19c560c2be51593183a51f727c
Mike15426378 / <Ravaga228>
2022-03-07 15:42:57 +0300

    v4 все работает 1 - но надо бы допилить присвоение роли 2 - и отображение кнопок в зависимости от роли

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,10 +3,7 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
@@ -27,31 +24,13 @@ public class AdminController {
     @GetMapping("")
     public String findAll(Model model, Principal principal) {
         User user = userService.findByUsername(principal.getName());
-        model.addAttribute("user", user);
+        model.addAttribute("admin", user);
         model.addAttribute("users", userService.findAll());
         return "admin";
     }
 
-    @GetMapping("/user-create")
-    public String createUserForm(User user) {
-        return "userCreate";
-    }
-
-    @PostMapping("/user-create")
-    public String createUser(User user) {
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/user-update/{id}")
-    public String updateUserForm(@PathVariable("id") long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-        return "userUpdate";
-    }
-
-    @PostMapping("/user-update")
-    public String updateUser(User user) {
+    @PostMapping("/user-save")
+    public String saveUser(User user) {
         userService.saveUser(user);
         return "redirect:/admin";
     }
@@ -60,5 +39,12 @@ public class AdminController {
     public String deleteUser(@PathVariable("id") long id) {
         userService.deleteById(id);
         return "redirect:/admin";
+    }
+
+    @GetMapping("/profile")
+    public String userInfoPage(Principal principal, Model model) {
+        User user = userService.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+        return "adminProfile";
     }
 }
